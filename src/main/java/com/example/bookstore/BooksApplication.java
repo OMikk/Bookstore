fb801817e4820d9eab00f5a7b13a3590b7fa7801
OMikk <54263275+OMikk@users.noreply.github.com>
2019-09-15 17:42:26 +0300

    Add files via upload

--- src/main/java/com/example/bookstore/BooksApplication.java
+++ src/main/java/com/example/bookstore/BooksApplication.java
@@ -1,13 +1,48 @@
 package com.example.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import com.example.bookstore.demo.Book;
+import com.example.bookstore.demo.BookRepository;
+import com.example.bookstore.demo.Category;
+import com.example.bookstore.demo.CategoryRepository;
+
+
+
 
 @SpringBootApplication
 public class BooksApplication {
+	private static final Logger log = LoggerFactory.getLogger(BooksApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BooksApplication.class, args);
 	}
-
+	
+	@Bean
+	public CommandLineRunner BookDemo(BookRepository repository, CategoryRepository crepository) {
+		return (args) -> {
+			log.info("save a couple of books");
+			crepository.save(new Category("Nature"));
+			crepository.save(new Category("Horror"));
+			
+			
+			repository.save(new Book("Norjan kalat", "Matti Mainio", "2015", "1234", "13.45"));
+			repository.save(new Book("Ihmeellinen luonto", "Jorma Jousimies", "2010", "1235", "15.00"));
+			
+			
+			
+			
+			log.info("fetch all books");
+			for (Book book  : repository.findAll()) {
+				log.info(book.toString());
+			}
+			
+		};
+	}
 }
---
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -0,0 +1,57 @@
+package com.example.bookstore.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import com.example.bookstore.demo.BookRepository;
+import com.example.bookstore.demo.Book;
+import com.example.bookstore.demo.CategoryRepository;
+@Controller
+public class BookController {
+	@Autowired
+	private BookRepository repository;
+	
+	@Autowired
+	private CategoryRepository crepository;
+	
+	@RequestMapping("/index")
+	public String index() {
+	return "This is the main page";
+	}
+	
+	@RequestMapping(value = {"/", "/booklist"})
+	public String booklist(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
+	}
+	
+	@RequestMapping(value = "/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		model.addAttribute("category", crepository.findAll());
+		return "addbook";
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
+	
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public String editBook(@PathVariable("id") Long bookId, Model model) {
+		repository.findById(bookId);
+		return "redirect:../booklist";
+	}
+	
+	
+}
